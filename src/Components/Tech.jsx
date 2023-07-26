c7f494d6f9dbedbc1fadbb7d8491f2f9a748e2ed
Karan Ketanbhai Dattani <karan.dce21@sot.pdpu.ac.in>
2023-07-26 23:00:52 +0530

    changes

--- src/Components/Tech.jsx
+++ src/Components/Tech.jsx
@@ -1,10 +1,52 @@
+// import { motion } from "framer-motion";
+// import { BallCanvas } from "./canvas";
+// import { SectionWrapper } from "../hoc";
+// import { technologies } from "../constants";
+// import { styles } from "../styles";
+// import { textVariant } from "../utils/motion";
+// import Marquee from "react-fast-marquee";
+// import "../Elements/Tech.css";
+
+// const Tech = () => {
+//   return (
+//     <>
+//       <motion.div variants={textVariant()} className="tech-content">
+//         <h2>Technologies.</h2>
+//         <p>My skills</p>
+//       </motion.div>
+
+//       <div className="skills" style={{ display: "flex" }}>
+//         <Marquee
+//           autoFill={true}
+//           pauseOnHover={true}
+//           speed={10}
+//           gradient={true}
+//           gradientColor={[236, 231, 225, 1]}
+//           gradientWidth={100}
+//         >
+//           {" "}
+//           {technologies.map((technology) => (
+//             <div className="skills-content" key={technology.name}>
+//               <div className="skill-text">
+//                 <h1>{technology.name}</h1>
+//               </div>
+//               <BallCanvas icon={technology.icon} />
+//             </div>
+//           ))}
+//         </Marquee>
+//       </div>
+//     </>
+//   );
+// };
+
+// export default SectionWrapper(Tech, "");
+
 import { motion } from "framer-motion";
-import { BallCanvas } from "./canvas";
 import { SectionWrapper } from "../hoc";
 import { technologies } from "../constants";
 import { styles } from "../styles";
 import { textVariant } from "../utils/motion";
-import Marquee from "react-fast-marquee";
+import { BallCanvas } from "./canvas";
 import "../Elements/Tech.css";
 
 const Tech = () => {
@@ -15,16 +57,8 @@ const Tech = () => {
         <p>My skills</p>
       </motion.div>
 
-      <div className="skills" style={{ display: "flex" }}>
-        <Marquee
-          autoFill={true}
-          pauseOnHover={true}
-          speed={10}
-          gradient={true}
-          gradientColor={[236, 231, 225, 1]}
-          gradientWidth={100}
-        >
-          {" "}
+      <div className="skills marquee-container">
+        <div className="marquee-content">
           {technologies.map((technology) => (
             <div className="skills-content" key={technology.name}>
               <div className="skill-text">
@@ -33,7 +67,7 @@ const Tech = () => {
               <BallCanvas icon={technology.icon} />
             </div>
           ))}
-        </Marquee>
+        </div>
       </div>
     </>
   );
